e28cc497c91fe62ac825ca60616593968c5d180e
wendeshiro <shiro12311@gmail.com>
2025-12-02 00:43:01 -0800

    Add; Report card component

--- app/page.js
+++ app/page.js
@@ -1,5 +1,17 @@
 import styles from "./page.module.css";
+import ReportCard from "./components/ReportCard";
 
 export default function Home() {
-  return <div className={styles.page}>On-Site Discrimination and Harassment</div>;
+  return (
+    <div className={styles.page}>
+      <ReportCard
+        tags={["Carpentry", "Discrimination"]}
+        title="On-Site Discrimination and Harassment"
+        location="4376 Gilpin Cres, Burnaby BC"
+        excerpt="During a worksite interaction, several workers engaged in repeated instances of discriminatory and harassing behavior directed toward a female coworker and others on site. The incident involved the use of derogatory language, exclusionary behavior, and comments that reinforced gender-based bias and hostility."
+        date="2025-11-04"
+        timestamp="10:15 AM"
+      />
+    </div>
+  );
 }
